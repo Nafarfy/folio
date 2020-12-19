ec76c99fdc805db1839c9f20271828a9532ea8fb
Your NameNafarfy <you@example.comthatsickdatsik@gmail.com>
2020-12-19 18:38:12 +0200

    media added

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,14 @@
 const burgerBtn = document.querySelector('.burger');
 const about = document.querySelector('#about-me');
-about.style.top = '-1500px';
+about.style.top = '-2500px';
 
 burgerBtn.addEventListener('click', () => {
-  if (about.style.top === '-1500px') {
+  if (about.style.top === '-2500px') {
     about.style.top = '0';
     burgerBtn.classList.remove('burger');
     burgerBtn.classList.add('burgerX');
   } else {
-    about.style.top = '-1500px';
+    about.style.top = '-2500px';
     burgerBtn.classList.remove('burgerX');
     burgerBtn.classList.add('burger');
   }
